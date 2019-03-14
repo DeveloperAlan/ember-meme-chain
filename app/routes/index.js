a3d6c0f5ad0dba94a106d80a5ed028b15063498d
alan <alanng95@hotmail.com>
2019-03-14 23:56:57 +1100

    got snowwrap and reddit to work, deleted environment js because confidential information

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,11 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+    redditApi: service(),
+
+    model() {
+        let result = this.redditApi.getDankMemes();
+        // console.log(result);
+    }
 });
--- app/services/reddit-api.js
+++ app/services/reddit-api.js
@@ -1,4 +1,31 @@
 import Service from '@ember/service';
+import { set } from '@ember/object';
+import snoowrap from "snoowrap";
+import config from '../config/environment';
 
 export default Service.extend({
+    init() {        
+        console.log("reddit-api is currently working");
+        this._super(...arguments);
+    },
+
+    async getDankMemes() {
+        if (!this.snoowrap) {
+            console.log(config)
+            let reddit = new snoowrap({
+                userAgent: config.APP.name,
+                clientId: config.APP.clientId,
+                clientSecret: config.APP.clientSecret,
+                refreshToken: config.APP.refreshToken,
+            })
+            set(this, 'snoowrap', reddit);
+        }
+        // console.log(this.snoowrap)
+        try {
+            console.log(this.snoowrap)
+            let result = this.snoowrap.getHot().then(console.log);
+        } catch(e) {
+            console.log(e)
+        }
+    }
 });
